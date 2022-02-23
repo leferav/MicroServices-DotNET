0b54a9cc289487221b1abe9fb790db402e22554c
“Leandro <leferav@gmail.com>
2022-02-23 00:42:50 -0300

    Inicio Metodo Post

--- RestWithASPNET/RestWithASPNET/Startup.cs
+++ RestWithASPNET/RestWithASPNET/Startup.cs
@@ -1,15 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestWithASPNET.Services.Implementations;
+using RestWithASPNET.Services;
 
 namespace RestWithASPNET
 {
@@ -27,6 +26,8 @@ namespace RestWithASPNET
         {
 
             services.AddControllers();
+
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
