6ecec2dac2eb15fb3c9a9a518757fa9bd7a01cbe
Leandro Ávila <leferav@gmail.com>
2022-11-21 15:23:15 -0300

    Projeto Completo

---
+++ CursoMicrosServices-DotNET/MicroServices/GeekShopping/GeekShopping.ProductAPI/Migrations/20221121172742_SeedProductDataTable.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "product",
+                keyColumn: "image_url",
+                keyValue: null,
+                column: "image_url",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "image_url",
+                table: "product",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "product",
+                keyColumn: "description",
+                keyValue: null,
+                column: "description",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "product",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "product",
+                keyColumn: "category_name",
+                keyValue: null,
+                column: "category_name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "category_name",
+                table: "product",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 2L, "T-shirt", "Description ID2", "C:\\Users\\LeandroAvila\\Downloads\\ShoppingImages\\1_super_mario.jpg", "Name02", 69.9m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 3L, "T-shirt", "Description ID3", "C:\\Users\\LeandroAvila\\Downloads\\ShoppingImages\\1_super_mario.jpg", "Name03", 69.9m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 4L, "T-shirt", "Description ID4", "C:\\Users\\LeandroAvila\\Downloads\\ShoppingImages\\1_super_mario.jpg", "Name04", 69.9m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "image_url",
+                table: "product",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "product",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "category_name",
+                table: "product",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ CursoMicrosServices-DotNET/MicroServices/GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() { }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            { 
+                Id = 2,
+                Name = "Name02",
+                Price = new decimal (69.9),
+                Description= "Description ID2",
+                ImageURL= "C:\\Users\\LeandroAvila\\Downloads\\ShoppingImages\\1_super_mario.jpg",
+                CategoryName= "T-shirt",
+
+            });            
+            modelBuilder.Entity<Product>().HasData(new Product
+            { 
+                Id = 3,
+                Name = "Name03",
+                Price = new decimal (69.9),
+                Description= "Description ID3",
+                ImageURL= "C:\\Users\\LeandroAvila\\Downloads\\ShoppingImages\\1_super_mario.jpg",
+                CategoryName= "T-shirt",
+
+            });            
+            modelBuilder.Entity<Product>().HasData(new Product
+            { 
+                Id = 4,
+                Name = "Name04",
+                Price = new decimal (69.9),
+                Description= "Description ID4",
+                ImageURL= "C:\\Users\\LeandroAvila\\Downloads\\ShoppingImages\\1_super_mario.jpg",
+                CategoryName= "T-shirt",
+
+            });
+        }
+    }
+}
